d1021e48915d2d3e70ce038e6e061f98ce47e3fd
DrEngineerOnMeth <152777243+DrEngineerOnMeth@users.noreply.github.com>
2024-02-09 21:53:14 -0600

    Unleashed Chaos upon Startup
    Refactored startup code to work better with many turtles

--- startup.lua
+++ startup.lua
@@ -1,4 +1,6 @@
 turtle.select(1);turtle.refuel()
+shell.run("set","motd.enable","false")
+str="Fuel:";print(str..tostring(turtle.getFuelLevel()))
 if disk.isPresent("bottom")==false then return end
 rootFiles=fs.list("/")
 files=fs.list("disk/turtles/")
@@ -10,6 +12,15 @@ for i,target in ipairs(files) do
 	path=fs.combine("disk/turtles/",target)
 	fs.copy(path,target)
 end
+if turtle.getFuelLevel()<10000 then
 turtle.suck(1)
 turtle.refuel()
+end
 turtle.up()
+os.sleep(1)
+turtle.up()
+while true do
+turtle.digDown()
+turtle.drop()
+os.sleep(1)
+end
